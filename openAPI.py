5ec21ae49b427e83b925976e324ae93fcbdedead
KHWoo <khw@khw-PC>
2019-05-02 12:08:22 +0900

    hi^^

--- openAPI.py
+++ openAPI.py
@@ -6,9 +6,9 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
 sys.syderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 
-url = 'http://data.ex.co.kr/exopenapi/restinfo/restConvList'
-key = '?ServiceKey=1ZUPc%2BJmDiSiYavXUDMa1%2BbIXXZlyQjE1y%2FPZpJ88HsW28VvHpu7Sc4SS8DFMCrJlufba3pK3sf2JQtSvQ80Gg%3D%3D'
-format = '&type=xml'
+url = 'http://know.kfri.go.kr/openapi/cyberLib/peridodicalIndexSearch.do?'
+key = 'keyValue=fZGmHhe%2BTsKPdIFP0icM0MT2qhJOXiIBCUOfpWsJdN8%3D'
+format = '&version=1.0&searchGbn=TITLE'
 
 req = ul.Request(url + key + format)
 
