64e7a2d6535c1a601a27aeed5492ccf8b023c233
KHWoo <khw@khw-PC>
2019-05-11 21:07:25 +0900

    도시코드 dictionary 저장

--- openAPI.py
+++ openAPI.py
@@ -13,10 +13,21 @@ url = 'http://openapi.tago.go.kr/openapi/service/BusSttnInfoInqireService/getCty
 request = urllib.request.Request(url + serviceKey)
 response = urllib.request.urlopen(request)
 rescode = response.getcode()
+cityCodeToStr = dict()
+cityStrToCode = dict()
 
 if rescode == 200:
     resbody = response.read()
     #print(resbody.decode('utf-8'))
     #Dom 객체를 통한 파싱
-    doc = parseString(resbody)  # 문자열 입력 파싱 함수, DOC객체 반환
-    print(doc.toxml())
+    doc = parseString(resbody)  # 문자열 입력 파싱 함수, DOC객체 반환1
+    #print(doc.toxml())
+    res = doc.childNodes #맨 처음 자식노드들, response
+    body = res[0].childNodes # 도시 번호가 들어있는 노드 header 와 body
+    items = body[1].childNodes # body에 도시 정보가 들어있으므로, 그걸 사용한다. items
+    for i in items[0].childNodes:
+        factor = i.childNodes
+        #print(factor[0].firstChild, factor[1].firstChild)
+        cityCodeToStr[factor[0].firstChild.data] = factor[1].firstChild.data
+        cityStrToCode[factor[1].firstChild.data] = factor[0].firstChild.data
+    #print(cityCodeToStr)
