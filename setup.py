e0955858fecfdbfe636fbb7f5f7fb80368fb172c
KHWoo <khw@khw-PC>
2019-06-15 14:26:29 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='spam',
+      version='1.0',
+py_modules=['openAPI', 'navermail', 'mystmplib', 'nearBusstop', 'noti', 'searchBus', 'selectMenu', 'teller', 'term', 'telnew']
+      )
